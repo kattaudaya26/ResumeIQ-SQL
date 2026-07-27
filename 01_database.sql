621bab5ae418baedb14e63b1d7df7e2bdecf4a83
Katta Udaya Lakshmi <udayalakshmikatta2608@gmail.com>
2026-07-27 19:04:31 +0530

    Update 01_database.sql

--- 01_database.sql
+++ 01_database.sql
@@ -1,11 +1,8 @@
 create database TalentHireDB;
 USE TalentHireDB;
 
--- Verify the selected database
 SELECT DATABASE() AS Current_Database;
 
--- Show all databases
 SHOW DATABASES;
 
--- Display confirmation message
 SELECT 'TalentHireDB created successfully.' AS Message;
