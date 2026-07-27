5d8c03d431c9c32677c0b9040388d5d7cfc430b6
Katta Udaya Lakshmi <udayalakshmikatta2608@gmail.com>
2026-07-27 19:06:27 +0530

    Update 03_constraints.sql

--- 03_constraints.sql
+++ 03_constraints.sql
@@ -1,14 +1,5 @@
--- ======================================================
--- File Name : 03_constraints.sql
--- Project   : AI Resume Screening & Candidate Management
--- Database  : TalentHireDB
--- ======================================================
-
 USE TalentHireDB;
 
--- -----------------------------
--- Candidates Table Constraints
--- -----------------------------
 
 ALTER TABLE Candidates
 MODIFY FullName VARCHAR(100) NOT NULL;
@@ -23,29 +14,17 @@ ALTER TABLE Candidates
 ADD CONSTRAINT CHK_Experience
 CHECK (ExperienceYears >= 0);
 
--- -----------------------------
--- Skills Table Constraints
--- -----------------------------
-
 ALTER TABLE Skills
 MODIFY SkillName VARCHAR(50) NOT NULL;
 
 ALTER TABLE Skills
 ADD CONSTRAINT UQ_Skill UNIQUE (SkillName);
 
--- -----------------------------
--- Companies Table Constraints
--- -----------------------------
-
 ALTER TABLE Companies
 MODIFY CompanyName VARCHAR(100) NOT NULL;
 
 ALTER TABLE Companies
 ADD CONSTRAINT UQ_Company UNIQUE (CompanyName);
-
--- -----------------------------
--- Job Roles Constraints
--- -----------------------------
 
 ALTER TABLE JobRoles
 ADD CONSTRAINT CHK_Salary
@@ -59,10 +38,6 @@ ALTER TABLE JobRoles
 ADD CONSTRAINT CHK_ExperienceRequired
 CHECK (ExperienceRequired >= 0);
 
--- -----------------------------
--- Applications Constraints
--- -----------------------------
-
 ALTER TABLE Applications
 MODIFY ApplicationDate DATE NOT NULL;
 
@@ -70,10 +45,6 @@ ALTER TABLE Applications
 MODIFY Status
 ENUM('Applied','Shortlisted','Rejected','Selected')
 DEFAULT 'Applied';
-
--- -----------------------------
--- Interviews Constraints
--- -----------------------------
 
 ALTER TABLE Interviews
 ADD CONSTRAINT CHK_Score
@@ -83,10 +54,6 @@ ALTER TABLE Interviews
 MODIFY Result
 ENUM('Pass','Fail','Pending')
 DEFAULT 'Pending';
-
--- -----------------------------
--- Resume Scores Constraints
--- -----------------------------
 
 ALTER TABLE ResumeScores
 ADD CONSTRAINT CHK_ATS
@@ -104,18 +71,10 @@ ALTER TABLE ResumeScores
 ADD CONSTRAINT CHK_Overall
 CHECK (OverallScore BETWEEN 0 AND 100);
 
--- -----------------------------
--- Final Selection Constraints
--- -----------------------------
-
 ALTER TABLE FinalSelection
 MODIFY OfferStatus
 ENUM('Accepted','Rejected','Pending')
 DEFAULT 'Pending';
-
--- -----------------------------
--- Verify Constraints
--- -----------------------------
 
 SHOW CREATE TABLE Candidates;
 SHOW CREATE TABLE Skills;
